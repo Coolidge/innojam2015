61a965162a5206322afea02de10c5aa1cae8b7f8
Tsafrir Sklarski <tsafrir.sklarski@sap.com>
2015-02-19 12:59:31 +0200

    android app

--- android/mobile/src/main/java/innojam/sap/com/eyelike/MainActivity.java
+++ android/mobile/src/main/java/innojam/sap/com/eyelike/MainActivity.java
@@ -1,5 +1,6 @@
 package innojam.sap.com.eyelike;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,28 +13,12 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onStartService();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onStartService() {
+        // Construct our Intent specifying the Service
+        Intent i = new Intent(this, EyeLikeIntentService.class);
+        startService(i);
     }
 }
